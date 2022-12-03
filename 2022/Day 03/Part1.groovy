3b4a935e10527ebc6b2a07e91cd30b75b4d999d8
Demis <demisselva@gmail.com>
2022-12-03 17:13:05 +0100

    Beautified Day 3

--- 2022/Day 03/Part1.groovy
+++ 2022/Day 03/Part1.groovy
@@ -1,1 +1,17 @@
-println new File("input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().last()
+//Single line
+println new File("/input.txt").getText().split("\r\n").collect{it.substring(0, (int)(it.size()/2)).split("(?!^)").flatten().intersect(it.substring((int)(it.size()/2)).split("(?!^)").flatten()).get(0).collect{Character.isUpperCase((char)it) ? ((int)it - (int)"A" + 27) : ((int)it - (int)"a" + 1)}}.flatten().sum()
+
+//Beautified
+println new File("/input.txt").getText()
+                .split("\r\n").collect{
+                    it.substring(0, (int)(it.size()/2))
+                    .split("(?!^)")
+                    .flatten()
+                    .intersect(
+                        it.substring((int)(it.size()/2))
+                        .split("(?!^)")
+                        .flatten()
+                    ).get(0).collect{
+                        Character.isUpperCase((char)it) ? ((int)it - (int)"A" + 27) : ((int)it - (int)"a" + 1)
+                    }
+                }.flatten().sum()
--- 2022/Day 03/Part2.groovy
+++ 2022/Day 03/Part2.groovy
@@ -1,1 +1,22 @@
-println new File("input.txt").getText().split("\r\n").collate(3).collect{it.get(0).split("(?!^)").flatten().intersect(it.get(1).split("(?!^)").flatten()).intersect(it.get(2).split("(?!^)").flatten()).get(0)}.collect{Character.isUpperCase((char)it) ? ((int)it - (int)"A" + 27) : ((int)it - (int)"a" + 1)}.sum()
+//Single line
+println new File("/input.txt").getText().split("\r\n").collate(3).collect{it.get(0).split("(?!^)").flatten().intersect(it.get(1).split("(?!^)").flatten()).intersect(it.get(2).split("(?!^)").flatten()).get(0)}.collect{Character.isUpperCase((char)it) ? ((int)it - (int)"A" + 27) : ((int)it - (int)"a" + 1)}.sum()
+
+//Beautified
+println new File("/input.txt").getText()
+                .split("\r\n")
+                .collate(3).collect{
+                    it.get(0)
+                    .split("(?!^)")
+                    .flatten()
+                    .intersect(
+                        it.get(1)
+                        .split("(?!^)")
+                        .flatten()
+                    ).intersect(
+                        it.get(2)
+                        .split("(?!^)")
+                        .flatten()
+                    ).get(0)
+                }.collect{
+                    Character.isUpperCase((char)it) ? ((int)it - (int)"A" + 27) : ((int)it - (int)"a" + 1)
+                }.sum()
