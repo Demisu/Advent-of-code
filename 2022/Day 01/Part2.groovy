2f95784cbe85babecd4628760ea870a24129ab09
Demis <demisselva@gmail.com>
2022-12-03 14:02:19 +0100

    Updated input file paths

--- 2022/Day 01/Part2.groovy
+++ 2022/Day 01/Part2.groovy
@@ -1,1 +1,1 @@
-println new File("/input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().takeRight(3).sum()
+println new File("input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().takeRight(3).sum()
--- 2022/Day 03/Part1.groovy
+++ 2022/Day 03/Part1.groovy
@@ -1,1 +1,1 @@
-println new File("/input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().last()
+println new File("input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().last()
