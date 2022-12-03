f23b840bf2e74f8db9ce4cc97426faea8348f58e
Demis <demisselva@gmail.com>
2022-12-03 17:07:12 +0100

    Beautified Day 2

--- 2022/Day 02/Part1.groovy
+++ 2022/Day 02/Part1.groovy
@@ -1,1 +1,15 @@
-println new File("/input.txt").getText().replaceAll("X|A", "0").replaceAll("Y|B", "1").replaceAll("Z|C", "2").split("\r\n").collect{it.split(" ").collect{it.toInteger()}}.collect{it.get(1) +1 +[2, 0, 1, 2, 0].get(it.get(1)-it.get(0)+2)*3}.sum()
+//Single line
+println new File("/input.txt").getText().replaceAll("X|A", "0").replaceAll("Y|B", "1").replaceAll("Z|C", "2").split("\r\n").collect{it.split(" ").collect{it.toInteger()}}.collect{it.get(1) + 1 + [2, 0, 1, 2, 0].get(it.get(1) - it.get(0) + 2) * 3}.sum()
+
+//Beautified
+println new File("/input.txt").getText()
+                .replaceAll("X|A", "0")
+                .replaceAll("Y|B", "1")
+                .replaceAll("Z|C", "2")
+                .split("\r\n").collect{
+                    it.split(" ").collect{
+                        it.toInteger()
+                    }
+                }.collect{
+                    it.get(1) + 1 + [2, 0, 1, 2, 0].get(it.get(1) - it.get(0) + 2) * 3
+                }.sum()
