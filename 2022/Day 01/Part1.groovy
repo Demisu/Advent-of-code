7e625c20701b8dd6068602cc619ade86bd1e0a95
Demis <demisselva@gmail.com>
2022-12-03 17:03:58 +0100

    Beautified Day 1

--- 2022/Day 01/Part1.groovy
+++ 2022/Day 01/Part1.groovy
@@ -1,1 +1,11 @@
+//Single line
 println new File("input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().last()
+
+//Beautified
+println new File("input.txt").getText()
+                .split("\r\n\r\n")
+                .collect{
+                    it.split("\r\n").collect{
+                        it.toInteger()
+                    }.sum()
+                }.sort().last()
--- 2022/Day 01/Part2.groovy
+++ 2022/Day 01/Part2.groovy
@@ -1,1 +1,11 @@
+//Single line
 println new File("input.txt").getText().split("\r\n\r\n").collect{it.split("\r\n").collect{it.toInteger()}.sum()}.sort().takeRight(3).sum()
+
+//Beautified
+println new File("input.txt").getText()
+                .split("\r\n\r\n")
+                .collect{
+                    it.split("\r\n").collect{
+                        it.toInteger()
+                    }.sum()
+                }.sort().takeRight(3).sum()
