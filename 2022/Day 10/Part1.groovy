1ceb1c39190a8785a4a0136ee51f5e74549110f2
Demis <demisselva@gmail.com>
2022-12-10 23:38:19 +0100

    Completed Day 10

---
+++ 2022/Day 10/Part1.groovy
@@ -0,0 +1,27 @@
+def input = new File("/uploads/input.txt").getText().split("\r\n")
+def resultMap = [:]
+resultMap.put(0, [1])
+
+for(int i = 0, cycle = 1; i < input.size(); i++, cycle++){
+    if(input[i].contains("addx")){
+        resultMap.put(cycle+3, 
+            resultMap.get(cycle+3) ?
+            (resultMap.get(cycle+3) + input[i].replaceAll("addx ", "").toInteger()) :
+            (input[i].replaceAll("addx ", "").toInteger())
+        )
+        cycle++
+    }
+    if(!resultMap.get(cycle-1)){
+        resultMap.put(cycle-1, [0])
+    }
+    if(!resultMap.get(cycle)){
+        resultMap.put(cycle, [0])
+    }
+}
+
+println resultMap.subMap(0..21).values().flatten().sum() * 20 +
+        resultMap.subMap(0..61).values().flatten().sum() * 60 +
+        resultMap.subMap(0..101).values().flatten().sum() * 100 +
+        resultMap.subMap(0..141).values().flatten().sum() * 140 +
+        resultMap.subMap(0..181).values().flatten().sum() * 180 +
+        resultMap.subMap(0..221).values().flatten().sum() * 220
---
+++ 2022/Day 10/Part2.groovy
@@ -0,0 +1,40 @@
+def input = new File("/uploads/input.txt").getText().split("\r\n")
+def resultMap = [:]
+resultMap.put(0, 1)
+def crtPixels = ["#"]
+def partialResult = 1
+
+for(int i = 0, cycle = 1; i < input.size(); i++, cycle++){
+    if(input[i].contains("addx")){
+        resultMap.put(cycle+3, 
+            resultMap.get(cycle+3) ?
+            (resultMap.get(cycle+3) + input[i].replaceAll("addx ", "").toInteger()) :
+            (input[i].replaceAll("addx ", "").toInteger())
+        )
+        if(!resultMap.get(cycle-1)){
+            resultMap.put(cycle-1, 0)
+        }
+        if(!resultMap.get(cycle)){
+            resultMap.put(cycle, 0)
+        }
+        partialResult = resultMap.subMap(0..(cycle+1)).values().sum()
+        crtPixels << (((cycle-1)%40 >= partialResult-1 && (cycle-1)%40 <= partialResult+1) ? "#" : ".")
+        cycle++
+    }
+    if(!resultMap.get(cycle-1)){
+        resultMap.put(cycle-1, 0)
+    }
+    if(!resultMap.get(cycle)){
+        resultMap.put(cycle, 0)
+    }
+    partialResult = resultMap.subMap(0..(cycle+1)).values().sum()
+    crtPixels << (((cycle-1)%40 >= partialResult-1 && (cycle-1)%40 <= partialResult+1) ? "#" : ".")
+}
+
+for(int i = 0; i < crtPixels.size(); i++){
+    if((i-1) % 40 == 0){
+        print "\n"+crtPixels[i]
+    } else {
+        print crtPixels[i]
+    }
+}
